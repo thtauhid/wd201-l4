1dfc50b90af0795ca04a230e2bbae3b712a71a5c
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2022-10-10 22:03:50 +0530

    Created test cases for todo

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -38,4 +38,57 @@ describe('ToDo List Test Suite', () => {
 		markAsComplete(0)
 		expect(all[0].completed).toBe(true)
 	})
+
+	test('Should retrieve overdue items', () => {
+		const overdueCount = all.filter((item) => {
+			return new Date(item.dueDate) < new Date()
+		}).length
+		add({
+			title: 'New overdue item',
+			dueDate: new Date('2020-01-01'),
+			completed: false,
+		})
+
+		const newOverdueCount = all.filter((item) => {
+			return new Date(item.dueDate) < new Date()
+		}).length
+
+		expect(newOverdueCount).toBe(overdueCount + 1)
+	})
+
+	test('Should retrieve due today items', () => {
+		const dueTodayCount = all.filter((item) => {
+			return item.dueDate === new Date().toLocaleDateString('en-CA')
+		}).length
+
+		add({
+			title: 'New due today item',
+			dueDate: new Date().toLocaleDateString('en-CA'),
+			completed: false,
+		})
+
+		const newDueTodayCount = all.filter((item) => {
+			return item.dueDate === new Date().toLocaleDateString('en-CA')
+		}).length
+
+		expect(newDueTodayCount).toBe(dueTodayCount + 1)
+	})
+
+	test('Should retrieve due later items', () => {
+		const dueLaterCount = all.filter((item) => {
+			return item.dueDate > new Date().toLocaleDateString('en-CA')
+		}).length
+
+		add({
+			title: 'New due later item',
+			dueDate: new Date('2023-12-31').toLocaleDateString('en-CA'),
+			completed: false,
+		})
+
+		const newDueLaterCount = all.filter((item) => {
+			return item.dueDate > new Date().toLocaleDateString('en-CA')
+		}).length
+
+		expect(newDueLaterCount).toBe(dueLaterCount + 1)
+	})
 })
